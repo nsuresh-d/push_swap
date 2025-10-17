36f2a7879eeea2dbbc72e022a01767f46249ace4
nsuresh-d <nitzdsuji@gmail.com>
2025-10-18 01:15:43 +0800

    made some typo errors.. changed it

--- Push_Swap/push_b_to_a.c
+++ Push_Swap/push_b_to_a.c
@@ -16,11 +16,12 @@ static void	set_target_b(t_stack_node *a, t_stack_node *b)
 {
 	t_stack_node	*current_a;
 	t_stack_node	*target_node;
-	long		best_match_index;
+	long			best_match_index;
 
 	while (b)
 	{
 		best_match_index = LONG_MAX;
+		target_node = NULL;
 		current_a = a;
 		while (current_a)
 		{
@@ -32,7 +33,7 @@ static void	set_target_b(t_stack_node *a, t_stack_node *b)
 			current_a = current_a->next;
 		}
 		if (best_match_index == LONG_MAX)
-			b->target->node = find_min(a);
+			b->target_node = find_min(a);
 		else
 			b->target_node = target_node;
 		b = b->next;
@@ -41,6 +42,6 @@ static void	set_target_b(t_stack_node *a, t_stack_node *b)
 void	init_nodes_b(t_stack_node *a, t_stack_node *b)
 {
 	current_index(a);
-	current_index(a);
+	current_index(b);
 	set_target_b(a, b);
 }
