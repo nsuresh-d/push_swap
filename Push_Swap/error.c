14aa075212042cd616f8b6edcfcc315b6a58ef70
nsuresh-d <nitzdsuji@gmail.com>
2025-10-18 01:24:52 +0800

    added a function to free split

--- Push_Swap/error.c
+++ Push_Swap/error.c
@@ -64,3 +64,17 @@ void	free_errors(t_stack_node **stack)
 	write(2, "Error\n", 6);
 	exit(1);
 }
+void	free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
